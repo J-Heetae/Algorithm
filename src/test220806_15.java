1154e0b25d84efccb6ecbdb2fc25215ec6b75d1a
goose <th1563@naver.com>
2022-08-07 11:39:00 +0900

    220806

---
+++ src/test220806_15.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//String_문자찾기
+public class test220806_15 {
+    public int solution(String str, char t) {
+        int answer=0;
+        str = str.toLowerCase();
+        t = Character.toLowerCase(t);
+//        for(int i=0; i<str.length(); i++) {
+//            if(str.indexOf(i) == t) answer++;
+//        }
+        // .toCharArray() : String을 char로 나누어 배열로 만들어줌
+        for(char s : str.toCharArray()) {
+            if(s == t) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        test220806_15 T = new test220806_15();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        System.out.print(T.solution(str, c));
+    }
+}
---
+++ src/test220806_16.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+//String_대소문자변환
+public class test220806_16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String answer = "";
+
+        /*for (int i = 0; i < str.length(); i++) {
+            char s = str.charAt(i);
+            if(Character.isUpperCase(s))
+                answer += Character.toLowerCase(s);
+            else
+                answer += Character.toUpperCase(s);
+        }*/
+
+        /*for (char c : str.toCharArray()) {
+            if(Character.isUpperCase(c))
+                answer += Character.toLowerCase(c);
+            else
+                answer += Character.toUpperCase(c);
+        }*/
+
+        //ASCII 아스키코드 이용
+        for (char c : str.toCharArray()) {
+            if(c>=97 && c<=122)
+                answer += (char)(c - 32);
+            else
+                answer += (char)(c + 32);
+        }
+
+       System.out.print(answer);
+    }
+}
