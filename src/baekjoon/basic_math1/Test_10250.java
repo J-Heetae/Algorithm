6f3c5982f5dbc2bed21e18b5cecbee5436d9cbfa
goose <th1563@naver.com>
2022-12-21 01:23:01 +0900

    20221221

---
+++ src/baekjoon/basic_math1/Test_10250.java
@@ -0,0 +1,22 @@
+package baekjoon.basic_math1;
+
+import java.util.Scanner;
+
+public class Test_10250 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i=0; i<num; i++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+
+            if(N % H == 0) {
+                System.out.println((H * 100) + (N / H));
+            } else {
+                System.out.println(((N % H) * 100) + ((N / H) + 1));
+            }
+        }
+    }
+}
