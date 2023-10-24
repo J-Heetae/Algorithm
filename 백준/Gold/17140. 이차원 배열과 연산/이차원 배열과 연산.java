f76fc7a31ba461cf45c8862ef47898ab97f5ac0a
Jeong TaeHee <th1563@naver.com>
2023-10-24 11:58:30 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 180 ms, Memory: 19380 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 class Main {
+	static final int R = 0;
+	static final int C = 1;
 
 	static boolean find = false;
 	static int answer = -1;
@@ -14,7 +16,6 @@ class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		row = Integer.parseInt(st.nextToken());
 		col = Integer.parseInt(st.nextToken());
@@ -29,13 +30,15 @@ class Main {
 			}
 		}
 
-		if(arr.length > row-1 && arr[0].length > col -1 && arr[row-1][col-1] == targetNum) {
+		isFind();
+		if(find) {
 			answer = 0;
 		} else {
 			int count = 0;
 			while(++count <= 100) {
-				if (arr.length >= arr[0].length) R();
-				else C();
+				if ((arr.length >= arr[0].length)) method(R);
+				else method(C);
+
 				if(find) {
 					answer = count;
 					break;
@@ -45,81 +48,81 @@ class Main {
 		System.out.println(answer);
 	}
 
-	private static void R() {
-		ArrayList<Integer>[] tmpList = new ArrayList[arr.length];
-		int maxSize = 0;
-		for(int i=0; i<arr.length; i++) {
-			tmpList[i] = new ArrayList<>();
-			int[] countArr = new int[101];
+	private static void method(int methodType) {
+		int length = (methodType == R) ? arr.length : arr[0].length;
 
-			int maxNum = 0;
-			for(int j=0; j<arr[i].length; j++) {
-				maxNum = Math.max(maxNum, arr[i][j]);
-				countArr[arr[i][j]]++;
-			}
-			ArrayList<Number> numberList = new ArrayList<>();
-			for(int j=1; j<=maxNum; j++) {
-				if(countArr[j] > 0) {
-					numberList.add(new Number(j, countArr[j]));
-				}
-			}
+		ArrayList<Integer>[] tmpList = new ArrayList[length];
+		for (int i = 0; i < tmpList.length; i++) {
+			tmpList[i] = new ArrayList<>();
+		}
+
+		int maxSize = 0;
+		for(int i=0; i<length; i++) {
+			ArrayList<Number> numberList = getNumberList(i, methodType);
+			maxSize = Math.max(maxSize, numberList.size());
 			Collections.sort(numberList);
-			int size = numberList.size();
-			for (Number cur : numberList) {
-				tmpList[i].add(cur.value);
-				tmpList[i].add(cur.count);
-			}
-			maxSize = Math.max(maxSize, size);
+			fillList(numberList, tmpList[i]);
 		}
-		int[][] tmpArr = new int[arr.length][Math.min(maxSize * 2, 100)];
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<Math.min(tmpList[i].size(),100); j++) {
-				tmpArr[i][j] = tmpList[i].get(j);
-			}
-		}
-		arr = tmpArr;
-		if(arr.length > row-1 && arr[0].length > col -1 && arr[row-1][col-1] == targetNum) {
+		changeArr(maxSize, tmpList, methodType);
+		isFind();
+	}
+
+	private static void isFind() {
+		if(arr.length > row - 1 && arr[0].length > col - 1 && arr[row - 1][col - 1] == targetNum) {
 			find = true;
 		}
 	}
 
-	private static void C() {
-		ArrayList<Integer>[] tmpList = new ArrayList[arr[0].length];
-		int maxSize = 0;
-		for(int i=0; i<arr[0].length; i++) {
-			tmpList[i] = new ArrayList<>();
-			int[] countArr = new int[101];
+	private static void fillList(ArrayList<Number> numberList, ArrayList<Integer> tmpList) {
+		for (Number cur : numberList) {
+			tmpList.add(cur.value);
+			tmpList.add(cur.count);
+		}
+	}
 
-			int maxNum = 0;
+	private static ArrayList<Number> getNumberList(int idx, int methodType) {
+		ArrayList<Number> numberList = new ArrayList<>();
+		int maxNum = 0;
+		int[] countArr = new int[101];
+
+		if(methodType == R) {
+			for (int j = 0; j < arr[idx].length; j++) {
+				countArr[arr[idx][j]]++;
+				maxNum = Math.max(maxNum, arr[idx][j]);
+			}
+		} else {
 			for(int j=0; j<arr.length; j++) {
-				int curNum = arr[j][i];
+				int curNum = arr[j][idx];
 				maxNum = Math.max(maxNum, curNum);
 				countArr[curNum]++;
 			}
-			ArrayList<Number> numberList = new ArrayList<>();
-			for(int j=1; j<=maxNum; j++) {
-				if(countArr[j] > 0) {
-					numberList.add(new Number(j, countArr[j]));
-				}
-			}
-			Collections.sort(numberList);
-			int size = numberList.size();
-			for (Number cur : numberList) {
-				tmpList[i].add(cur.value);
-				tmpList[i].add(cur.count);
-			}
-			maxSize = Math.max(maxSize, size);
 		}
-		int[][] tmpArr = new int[Math.min(maxSize * 2, 100)][arr[0].length];
-		for(int i=0; i<arr[0].length; i++) {
-			for(int j=0; j<Math.min(tmpList[i].size(),100); j++) {
-				tmpArr[j][i] = tmpList[i].get(j);
+
+		for(int j=1; j<=maxNum; j++) {
+			if(countArr[j] > 0) {
+				numberList.add(new Number(j, countArr[j]));
+			}
+		}
+		return numberList;
+	}
+
+	private static void changeArr(int maxSize, ArrayList<Integer>[] tmpList, int methodType) {
+		int row = (methodType == R) ? arr.length : Math.min(maxSize * 2, 100);
+		int col = (methodType == R) ? Math.min(maxSize * 2, 100) : arr[0].length;
+
+		int[][] tmpArr = new int[row][col];
+
+		int limit = (methodType == R) ? row : col;
+		for (int i = 0; i < limit; i++) {
+			for (int j = 0; j < Math.min(tmpList[i].size(), 100); j++) {
+				if(methodType == R) {
+					tmpArr[i][j] = tmpList[i].get(j);
+				} else {
+					tmpArr[j][i] = tmpList[i].get(j);
+				}
 			}
 		}
 		arr = tmpArr;
-		if(arr.length > row-1 && arr[0].length > col -1 && arr[row-1][col-1] == targetNum) {
-			find = true;
-		}
 	}
 
 	private static class Number implements Comparable<Number> {
