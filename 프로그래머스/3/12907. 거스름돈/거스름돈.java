6f07709fabf4b20816cb9d7c40b1e800b53ac4a7
Jeong TaeHee <th1563@naver.com>
2023-10-31 23:09:19 +0900

    [level 3] Title: 거스름돈, Time: 11.38 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int m : money) {
+            for (int i = 1; i <= n; i++) {
+                if (i < m) {
+                    continue;
+                }
+                dp[i] += dp[i - m];
+            }
+        }
+        return dp[n];
+    }
+}
