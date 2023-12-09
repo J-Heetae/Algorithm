752151860ada0b26089a8d03a04f572625df26c5
Jeong TaeHee <th1563@naver.com>
2023-12-09 23:15:44 +0900

    [Gold III] Title: 게임 개발, Time: 292 ms, Memory: 22216 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static int[] degree, timeArr, answer;
+    static ArrayList<Integer>[] edgeList;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        answer = new int[N+1];
+        timeArr = new int[N+1];
+        degree = new int[N+1];
+        edgeList = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) {
+            edgeList[i] = new ArrayList();
+        }
+
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int time = Integer.parseInt(st.nextToken());
+            timeArr[i] = time;
+
+            int before = Integer.parseInt(st.nextToken());
+            while(before != -1) {
+                degree[i]++;
+                edgeList[before].add(i);
+                before = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Queue<Integer> que = new LinkedList<>();
+        for(int i=1; i<=N; i++) {
+            if(degree[i] == 0) {
+                que.offer(i);
+            }
+        }
+
+        while(!que.isEmpty()) {
+            int cur = que.poll();
+            build(cur);
+        }
+
+        for(int i=1; i<=N; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+
+    private static void build(int num) {
+        answer[num] += timeArr[num];
+
+        for(int next : edgeList[num]) {
+            degree[next]--;
+            answer[next] = Math.max(answer[next], answer[num]);
+            if(degree[next] == 0) {
+                build(next);
+            }
+        }
+    }
+}
