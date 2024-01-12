d3ab4033890669d4dd3e535a49749f8a001c7650
Jeong TaeHee <th1563@naver.com>
2024-01-12 11:24:06 +0900

    [level Lv.3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/Lv.3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/Lv.3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,12 +1,11 @@
--- 코드를 입력하세요
 SELECT U.USER_ID, U.NICKNAME,
-       (CITY || ' ' || U.STREET_ADDRESS1 || ' ' || U.STREET_ADDRESS2) AS 전체주소,
-       REGEXP_REPLACE(U.TLNO, '(.{3})(.+)(.{4})', '\1-\2-\3') AS 전화번호
+       (U.CITY || ' ' || U.STREET_ADDRESS1 || ' ' || U.STREET_ADDRESS2) AS 전체주소,
+       TO_CHAR(SUBSTR(U.TLNO, 1, 3) || '-' || SUBSTR(U.TLNO, 4, 4) || '-' || SUBSTR(U.TLNO, 8, 4)) AS 전화번호
 FROM USED_GOODS_USER U
 JOIN (
-    SELECT B.WRITER_ID, COUNT(*)
+    SELECT B.WRITER_ID, COUNT(1)
     FROM USED_GOODS_BOARD B
     GROUP BY B.WRITER_ID
-    HAVING COUNT(*) >= 3
+    HAVING COUNT(1) >= 3
 ) T ON U.USER_ID = T.WRITER_ID
 ORDER BY U.USER_ID DESC;
