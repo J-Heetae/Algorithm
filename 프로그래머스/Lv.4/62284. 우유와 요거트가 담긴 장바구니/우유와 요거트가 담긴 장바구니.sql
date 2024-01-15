711e27dcdb8707edafaf1fc344c5c5c3af28952f
Jeong TaeHee <th1563@naver.com>
2024-01-15 14:50:16 +0900

    [level Lv.4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/Lv.4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/Lv.4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,8 +1,14 @@
-SELECT B.CART_ID
-FROM CART_PRODUCTS B
-WHERE B.CART_ID IN (
-    SELECT A.CART_ID
-    FROM CART_PRODUCTS A
-    WHERE A.NAME = 'Milk'
-) AND B.NAME = 'Yogurt'
-ORDER BY B.CART_ID;
+SELECT CART_ID
+FROM
+(
+    SELECT CART_ID
+         , SUM(CASE WHEN NAME in ('Milk')THEN 1 ELSE 0 END) AS milk_cnt
+         , SUM(CASE WHEN NAME in ('Yogurt')THEN 1 ELSE 0 END) AS yogurt_cnt
+        FROM CART_PRODUCTS
+        WHERE 1=1
+        GROUP BY CART_ID
+) T
+WHERE 1=1
+  AND milk_cnt > 0
+  AND yogurt_cnt > 0
+ORDER BY CART_ID;
