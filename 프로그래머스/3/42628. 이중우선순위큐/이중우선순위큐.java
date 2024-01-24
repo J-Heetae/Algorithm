e547e7a8ade9d265dc84cdc05a37da0bd7d80803
Jeong TaeHee <th1563@naver.com>
2024-01-24 14:10:50 +0900

    [level 3] Title: 이중우선순위큐, Time: 1.15 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,40 +1,35 @@
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Solution {
     public int[] solution(String[] operations) {
-        List<Integer> list = new LinkedList<>();
+        PriorityQueue<Integer> asc = new PriorityQueue<>();
+        PriorityQueue<Integer> desc = new PriorityQueue<>(Collections.reverseOrder());
 
         StringTokenizer st;
-        for(String operation : operations) {
+        for (String operation : operations) {
             st = new StringTokenizer(operation);
 
-            String order = st.nextToken();
+            String command = st.nextToken();
             int value = Integer.parseInt(st.nextToken());
 
-            if(order.equals("I")) {
-                list.add(value);
-                Collections.sort(list);
-                continue;
-            }
-
-            if(value == -1 && !list.isEmpty()) {
-                list.remove(0);
-            } else if(value == 1 && !list.isEmpty()) {
-                list.remove(list.size() - 1);
+            switch (command) {
+                case "I":
+                    asc.offer(value);
+                    desc.offer(value);
+                    break;
+                case "D":
+                    if (asc.isEmpty())
+                        break;
+                    if (value == 1)
+                        asc.remove(desc.poll());
+                    else
+                        desc.remove(asc.poll());
             }
         }
 
-        int[] answer = {0, 0};
-        if(!list.isEmpty()) {
-            answer[0] = list.get(list.size() - 1);
-            answer[1] = list.get(0);
-        }
-        return answer;
+        if(asc.isEmpty()) return new int[] {0, 0};
+        return new int[] {desc.peek(), asc.peek()};
     }
 }
