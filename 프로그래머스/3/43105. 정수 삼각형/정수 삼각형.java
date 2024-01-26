942e5d380fffbf2678e804a123d151efe14131ac
Jeong TaeHee <th1563@naver.com>
2024-01-26 12:59:08 +0900

    [level 3] Title: 정수 삼각형, Time: 11.45 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,24 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        
-        int[][] copy = new int[triangle.length][triangle.length+1];
-        
-        int max = Integer.MIN_VALUE;
+        int[][] copy = new int[triangle.length][];
+        for(int i=0; i<triangle.length; i++) {
+            copy[i] = new int[triangle[i].length];
+        }
         
         copy[0][0] = triangle[0][0];
-        for(int i=0; i<triangle.length; i++) {
-            if(i < triangle.length -1) {
-                for(int j=0; j<triangle[i].length; j++) {
-                    copy[i+1][j] = Math.max(copy[i+1][j], copy[i][j] + triangle[i+1][j]);
-                    copy[i+1][j+1] = Math.max(copy[i+1][j+1], copy[i][j] + triangle[i+1][j+1]);
-                }
-            } else {
-                for(int j=0; j<triangle[i].length; j++) {
-                    max = Math.max(max, copy[i][j]);
-                }
+        for(int i=0; i<triangle.length - 1; i++) {
+            for(int j=0; j<triangle[i].length; j++) {
+                copy[i+1][j] = Math.max(copy[i+1][j], copy[i][j] + triangle[i+1][j]);
+                copy[i+1][j+1] = Math.max(copy[i+1][j+1], copy[i][j] + triangle[i+1][j+1]);
             }
         }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<triangle[triangle.length - 1].length; i++) {
+            max = Math.max(max, copy[triangle.length - 1][i]);
+        }
+
         return max;
     }
 }
