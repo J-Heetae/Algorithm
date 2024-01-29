9664feb773d3c9ff72f5a289057c638f0bb1f4d0
Jeong TaeHee <th1563@naver.com>
2024-01-29 11:32:27 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,59 +1,35 @@
 class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
+        s.solution(3, new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}});
 
-        int solution = s.solution(5,
-            new int[][] {{1, 0, 0, 0, 1}, {0, 1, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 1, 1, 1}, {1, 0, 0, 1, 1}});
-
-        System.out.println(solution);
     }
 
-    static int[] networks;
+    static int[] status;
+    static int[][] connected;
+    static int networkNumber = 1;
+    static int computerNumber;
+
     public int solution(int n, int[][] computers) {
-        networks = new int[n];
+        computerNumber = n;
+        status = new int[n];
+        connected = computers;
+
         for(int i=0; i<n; i++) {
-            networks[i] = i;
-        }
-
-        for(int cur=0; cur<n; cur++) {
-            for(int next=0; next<n; next++) {
-                if(cur != next && computers[cur][next] == 1) {
-                    connect(cur, next);
-                }
+            if(status[i] == 0) {
+                connect(i, networkNumber++);
             }
         }
 
-        int[] count = new int[n];
-        for(int idx=0; idx<networks.length; idx++) {
-            count[getNetwork(idx)]++;
-        }
-
-        int answer = 0;
-        for(int c : count) {
-            if(c > 0) {
-                answer++;
-            }
-        }
-        return answer;
+        return networkNumber - 1;
     }
 
-    public void connect(int a, int b) {
-        int networkA = getNetwork(a);
-        int networkB = getNetwork(b);
-
-        if(networkA != networkB) {
-            if(networkA < networkB) {
-                networks[networkB] = networkA;
-            } else {
-                networks[networkA] = networkB;
+    void connect(int cur, int network) {
+        for(int next=0; next<computerNumber; next++) {
+            if(connected[cur][next] == 1 && status[next] == 0) {
+                status[next] = network;
+                connect(next, network);
             }
         }
-    }
-
-    public int getNetwork(int a) {
-        if(networks[a] == a) {
-            return a;
-        }
-        return networks[a] = getNetwork(networks[a]);
     }
 }
