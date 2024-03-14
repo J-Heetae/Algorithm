a53a1d7876dee96083c9e6db090003b557c39235
Jeong TaeHee <th1563@naver.com>
2024-03-14 11:06:03 +0900

    [level 3] Title: 부대복귀, Time: 69.49 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        ArrayList<ArrayList<Integer>> roadList = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            roadList.add(new ArrayList<>());
+        }
+
+        for (int[] road : roads) {
+            roadList.get(road[0]).add(road[1]);
+            roadList.get(road[1]).add(road[0]);
+        }
+
+        int[] distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> que = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        que.offer(new int[] {destination, 0});
+
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            int curArea = cur[0];
+            int curDistance = cur[1];
+
+            if (distance[curArea] < curDistance)
+                continue;
+
+            distance[curArea] = curDistance;
+
+            for (int next : roadList.get(curArea)) {
+                if (distance[next] < curDistance + 1)
+                    continue;
+                que.offer(new int[] {next, curDistance + 1});
+            }
+        }
+
+        int[] result = new int[sources.length];
+        for (int i = 0; i < sources.length; i++) {
+            int curDistance = distance[sources[i]];
+            result[i] = (curDistance == Integer.MAX_VALUE) ? -1 : curDistance;
+        }
+        return result;
+    }
+}
