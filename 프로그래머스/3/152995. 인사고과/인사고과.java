1b7e365c9decf069f63db2e058fba07d0a0153b6
Jeong TaeHee <th1563@naver.com>
2024-03-21 13:32:42 +0900

    [level 3] Title: 인사고과, Time: 103.86 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -1,66 +1,27 @@
 import java.util.Arrays;
 
 class Solution {
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        s.solution(new int[][]{{2, 2}, {1, 4}, {3, 2}, {3, 2}, {2, 1}});
-    }
-
-    static final int MAX_SCORE = 100_000;
     public int solution(int[][] scores) {
-        int length = scores.length;
-        int[] wanhoScore = scores[0];
+        final int[] WANHO = scores[0];
+        final int WANHO_SUM = WANHO[0] + WANHO[1];
 
         Arrays.sort(scores, (a, b) -> {
             if (a[0] == b[0])
-                return Integer.compare(b[1], a[1]);
-            return Integer.compare(b[0], a[0]);
+                return a[1] - b[1];
+            return b[0] - a[0];
         });
 
-        int idx = 100_001;
-        int[] maxArr = new int[MAX_SCORE + 1];
-        Arrays.fill(maxArr, Integer.MIN_VALUE);
-        for(int i=0; i<length; i++) {
-            if (scores[i][0] < idx) {
-                idx = scores[i][0];
-                maxArr[scores[i][0]] = scores[i][1];
-            }
-        }
-
-        int maxNum = scores[0][0];
-        int curNum = scores[0][0];
-        boolean curOut = false;
-        boolean[] out = new boolean[length];
-        for(int i=0; i<length; i++) {
-            if(scores[i][0] == curNum && curOut) {
-                if(scores[i][0] == wanhoScore[0] && scores[i][1] == wanhoScore[1])
-                    return -1;
-                out[i] = true;
-                continue;
-            }
-
-            if(scores[i][0] < curNum) {
-                curNum = scores[i][0];
-                curOut = false;
-            }
-
-            for(int j=curNum+1; j<=maxNum; j++) {
-                if(scores[i][1] < maxArr[j]) {
-                    if(scores[i][0] == wanhoScore[0] && scores[i][1] == wanhoScore[1])
-                        return -1;
-                    out[i] = true;
-                    curOut = true;
-                    break;
-                }
-            }
-        }
-
-        int wanhoSum = wanhoScore[0] + wanhoScore[1];
         int rank = 1;
-        for(int i=0; i<length; i++) {
-            if (!out[i] && scores[i][0] + scores[i][1] > wanhoSum)
+        int max = scores[0][1];
+        for (int[] score : scores) {
+            if(score[1] < max) {
+                if(Arrays.equals(score, WANHO))
+                    return -1;
+                continue;
+            } else if (score[0] + score[1] > WANHO_SUM){
                 rank++;
+            }
+            max = score[1];
         }
         return rank;
     }
