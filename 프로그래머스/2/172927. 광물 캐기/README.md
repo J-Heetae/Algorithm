da731a9330b8b6ec22569e63526d4c76d1bbeee5
Jeong TaeHee <th1563@naver.com>
2024-04-29 10:55:01 +0900

    [level 2] Title: 광물 캐기, Time: 0.09 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
 
 ### 성능 요약
 
-메모리: 77 MB, 시간: 0.14 ms
+메모리: 76.3 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 26일 14:07:12
+2024년 04월 29일 10:55:03
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,6 +1,57 @@
 class Solution {
+    final int[][] fatigueArr = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+    int minFatigue = Integer.MAX_VALUE;
+    int[] picks;
+    int[] minerals;
+
     public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-        return answer;
+        this.picks = picks;
+        this.minerals = new int[minerals.length];
+        for (int i = 0; i < minerals.length; i++) {
+            switch (minerals[i]) {
+                case "diamond":
+                    this.minerals[i] = 0;
+                    break;
+                case "iron":
+                    this.minerals[i] = 1;
+                    break;
+                case "stone":
+                    this.minerals[i] = 2;
+            }
+        }
+        mine(0, 0);
+        return minFatigue;
+    }
+
+    boolean pickExhaustion() {
+        for(int i = 0; i < picks.length; i++) {
+            if(picks[i] != 0)
+                return false;
+        }
+        return true;
+    }
+
+    void mine(int fatigue, int mineralIdx) {
+        if (fatigue >= minFatigue)
+            return;
+
+        if (mineralIdx == minerals.length || pickExhaustion()) {
+            minFatigue = Math.min(minFatigue, fatigue);
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (picks[i] > 0) {
+                picks[i]--;
+                int tempFatiue = fatigue;
+                int nextMineralIdx = Math.min(minerals.length, mineralIdx + 5);
+                for (int j = mineralIdx; j < nextMineralIdx; j++) {
+                    tempFatiue += fatigueArr[i][minerals[j]];
+                }
+                mine(tempFatiue, nextMineralIdx);
+                picks[i]++;
+            }
+        }
+
     }
 }
