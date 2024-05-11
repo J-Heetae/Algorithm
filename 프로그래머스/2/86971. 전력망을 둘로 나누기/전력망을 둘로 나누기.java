28ead28750df665053218df9bc17a6c995b67bd3
Jeong TaeHee <th1563@naver.com>
2024-05-11 13:03:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.58 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,48 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Solution {
 
     int n;
-    int minDiff = Integer.MAX_VALUE;
-    boolean[][] wireArr;
+    ArrayList<ArrayList<Integer>> wireList;
     boolean[] visited;
 
     public int solution(int n, int[][] wires) {
         this.n = n;
-        wireArr = new boolean[n + 1][n + 1];
+        wireList = new ArrayList<>();
         visited = new boolean[n + 1];
 
+        for(int i=0; i<=n; i++) {
+            wireList.add(new ArrayList<>());
+        }
+
         for (int[] wire : wires) {
-            int tower1 = wire[0];
-            int tower2 = wire[1];
-            wireArr[tower1][tower2] = true;
-            wireArr[tower2][tower1] = true;
+            wireList.get(wire[0]).add(wire[1]);
+            wireList.get(wire[1]).add(wire[0]);
         }
 
         int minDiff = Integer.MAX_VALUE;
         for (int[] wire : wires) {
-            int tower1 = wire[0];
-            int tower2 = wire[1];
-            wireArr[tower1][tower2] = false;
-            wireArr[tower2][tower1] = false;
-            visited[tower1] = true;
-            visited[tower2] = true;
-            int aCount = getCount(tower1);
-            int bCount = getCount(tower2);
-            visited[tower1] = false;
-            visited[tower2] = false;
-            minDiff = Math.min(minDiff, Math.abs(aCount - bCount));
-            wireArr[tower1][tower2] = true;
-            wireArr[tower2][tower1] = true;
+            wireList.get(wire[0]).remove((Integer) wire[1]);
+            wireList.get(wire[1]).remove((Integer) wire[0]);
+
+            int count = getCount(wire[0]);
+            minDiff = Math.min(minDiff, Math.abs(n - count * 2));
+
+            wireList.get(wire[0]).add(wire[1]);
+            wireList.get(wire[1]).add(wire[0]);
+
+            Arrays.fill(visited, false);
         }
         return minDiff;
     }
 
     int getCount(int cur) {
+        visited[cur] = true;
         int count = 1;
-        for(int i=1; i<=n; i++) {
-            if (wireArr[cur][i] && !visited[i]) {
-                visited[i] = true;
-                count += getCount(i);
-                visited[i] = false;
+        for (Integer next : wireList.get(cur)) {
+            if(!visited[next]) {
+                count += getCount(next);
             }
         }
         return count;
