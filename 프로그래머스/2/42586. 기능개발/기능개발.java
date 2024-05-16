0e9556a7d87784d00d4f17273cca0303acf7122a
Jeong TaeHee <th1563@naver.com>
2024-05-16 12:28:10 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(new int[] {93, 30, 55}, new int[] {1, 30, 5});
+    }
+    public int[] solution(int[] progresses, int[] speeds) {
+        int length = progresses.length;
+        int[] day = new int[length];
+
+        for (int i = 0; i < progresses.length; i++) {
+            int curProgress = progresses[i];
+            int curSpeed = speeds[i];
+
+            int curLeft = 100 - curProgress;
+            int remain = curLeft % curSpeed;
+            day[i] = curLeft / curSpeed + ((remain != 0) ? 1 : 0);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int before = day[0];
+        int count = 1;
+        for(int i=1; i<length; i++) {
+            if(day[i] > before) {
+                list.add(count);
+                before = day[i];
+                count = 1;
+                continue;
+            }
+            count++;
+        }
+        list.add(count);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
