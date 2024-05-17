ea4ecad07ee7d44224e7c3724c664372dc755a5d
Jeong TaeHee <th1563@naver.com>
2024-05-17 11:19:10 +0900

    [level 2] Title: 무인도 여행, Time: 0.44 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -4,11 +4,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        s.solution(new String[] {"X591X", "X1X5X", "X231X", "1XXX1"});
-    }
-
     public int[] solution(String[] maps) {
         int row = maps.length;
         int col = maps[0].length();
