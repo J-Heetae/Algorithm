94d38b2eb38caa34cf60601648fca91ecbca3a8c
Jeong TaeHee <th1563@naver.com>
2024-06-10 11:27:44 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 26.08 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(String s) {
+        int size = s.length();
+        int maxLength = 1;
+
+        boolean[][] dp = new boolean[size][size];
+
+        for (int i = 0; i < size; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < size - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                maxLength = 2;
+            }
+        }
+
+        for (int length = 3; length <= size; length++) {
+            for (int i = 0; i < size - length + 1; i++) {
+                int j = i + length - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    maxLength = length;
+                }
+            }
+        }
+        return maxLength;
+    }
+}
