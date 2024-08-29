b917b08cc4394e20d71d55490c261f8ad0776885
Jeong TaeHee <th1563@naver.com>
2024-08-29 17:48:51 +0900

    [Platinum III] Title: System Engineer, Time: 344 ms, Memory: 32204 KB -BaekjoonHub

--- 백준/Platinum/3736. System Engineer/System Engineer.java
+++ 백준/Platinum/3736. System Engineer/System Engineer.java
@@ -2,11 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
 
@@ -15,57 +23,100 @@ public class Main {
             if (line.isEmpty()) continue;
 
             int n = Integer.parseInt(line);
-            List<Integer>[] jobToServers = new ArrayList[n];
+
+            HopcroftKarp hopcroftKarp = new HopcroftKarp(n);
 
             for (int i = 0; i < n; i++) {
                 line = br.readLine().trim();
                 StringTokenizer st = new StringTokenizer(line, ": ()");
                 int job = Integer.parseInt(st.nextToken());
                 int numServers = Integer.parseInt(st.nextToken());
-                jobToServers[job] = new ArrayList<>();
-
                 for (int j = 0; j < numServers; j++) {
-                    jobToServers[job].add(Integer.parseInt(st.nextToken()));
+                    hopcroftKarp.addEdge(job, Integer.parseInt(st.nextToken()));
                 }
             }
-
-            System.out.println(maximumMatching(jobToServers, n));
+            int maxMatching = hopcroftKarp.hopcroftKarp();
+            sb.append(maxMatching).append("\n");
         }
+        System.out.println(sb);
     }
 
-    private static int maximumMatching(List<Integer>[] jobToServers, int n) {
-        int[] pairJob = new int[n];
-        int[] pairServer = new int[2 * n];
-        boolean[] visited = new boolean[n];
+    static class HopcroftKarp {
+        private static final int NIL = 0;
+        private static final int INF = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            pairJob[i] = -1;
-        }
-        for (int i = 0; i < 2 * n; i++) {
-            pairServer[i] = -1;
-        }
+        int n, start;
+        ArrayList<Integer>[] adj;
+        int[] pair, dist;
 
-        int matching = 0;
-        for (int i = 0; i < n; i++) {
-            visited = new boolean[n];
-            if (dfs(i, jobToServers, pairJob, pairServer, visited)) {
-                matching++;
+        public HopcroftKarp(int n) {
+            this.n = n;
+            this.start = 2 * n;
+            this.pair = new int[2 * n];
+            this.dist = new int[2 * n + 1];
+            this.adj = new ArrayList[2 * n];
+            for (int i = 0; i < 2 * n; i++) {
+                adj[i] = new ArrayList<>();
             }
         }
-        return matching;
-    }
 
-    private static boolean dfs(int job, List<Integer>[] jobToServers, int[] pairJob, int[] pairServer, boolean[] visited) {
-        if (visited[job]) return false;
-        visited[job] = true;
-
-        for (int server : jobToServers[job]) {
-            if (pairServer[server] == -1 || dfs(pairServer[server], jobToServers, pairJob, pairServer, visited)) {
-                pairJob[job] = server;
-                pairServer[server] = job;
-                return true;
-            }
+        public void addEdge(int job, int server) {
+            this.adj[job].add(server);
         }
-        return false;
+
+        private boolean bfs() {
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < n; i++) {
+                if (pair[i] == start) {
+                    queue.offer(i);
+                    dist[i] = 0;
+                } else {
+                    dist[i] = INF;
+                }
+            }
+            dist[start] = INF;
+            while (!queue.isEmpty()) {
+                int job = queue.poll();
+                if (dist[job] < dist[start]) {
+                    for (Integer server : adj[job]) {
+                        if (dist[pair[server]] == INF) {
+                            dist[pair[server]] = dist[job] + 1;
+                            queue.offer(pair[server]);
+                        }
+                    }
+                }
+            }
+            return dist[start] != INF;
+        }
+
+        private boolean dfs(int job) {
+            if (job != start) {
+                for (Integer server : adj[job]) {
+                    if (dist[pair[server]] == dist[job] + 1) {
+                        if (dfs(pair[server])) {
+                            pair[job] = server;
+                            pair[server] = job;
+                            return true;
+                        }
+                    }
+                }
+                dist[job] = INF;
+                return false;
+            }
+            return true;
+        }
+
+        public int hopcroftKarp() {
+            Arrays.fill(pair, start);
+            int matching = 0;
+            while (bfs()) {
+                for (int i = 0; i < n; i++) {
+                    if (pair[i] == start && dfs(i)) {
+                        matching++;
+                    }
+                }
+            }
+            return matching;
+        }
     }
 }
