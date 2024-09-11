5c222c7f854164cee92d54833c7890b675ef5511
Jeong TaeHee <th1563@naver.com>
2024-09-11 10:16:02 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14152 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int result = (A + B) * (A - B);
+        System.out.println(result);
+    }
+}
