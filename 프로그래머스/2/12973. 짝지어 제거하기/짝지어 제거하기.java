00899b8e5f9fbb24f2a29933ee4b6184aa0e5493
Jeong TaeHee <th1563@naver.com>
2024-10-31 14:58:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 66.61 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            if(stack.isEmpty()) {
+                stack.push(s.charAt(i));
+                continue;
+            }
+            
+            if(stack.peek() != s.charAt(i)) {
+                stack.push(s.charAt(i));
+            } else {
+                stack.pop();
+            }
+        }
+        return (stack.isEmpty()) ? 1 : 0;
+    }
+}
