c6487b110cec3eac371b1378691c3a7146f9befa
Jeong TaeHee <th1563@naver.com>
2024-12-02 16:28:10 +0900

    Title: 고대 문명 유적 탐사, Time: 156ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 241202/고대 문명 유적 탐사/ancient-ruin-exploration.java
+++ 241202/고대 문명 유적 탐사/ancient-ruin-exploration.java
@@ -2,212 +2,181 @@ import java.util.*;
 
 public class Main {
 
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    final static int N = 5;
+    final static int[] DX = {-1, -1, 0, 1, 1, 1, 0, -1};
+    final static int[] DY = {0, 1, 1, 1, 0, -1, -1, -1};
 
-    static int[][] wall;
-    static int[] next;
-    static int nextIdx;
-
-    static int maxCount;
+    static int[][] map;
+    static int[] spare;
+    static int spareIdx;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         int m = sc.nextInt();
         
-        wall = new int[5][5];
-        for(int i=0; i<5; i++) {
-            for(int j=0; j<5; j++) {
-                wall[i][j] = sc.nextInt();
+        map = new int[N][N];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                map[i][j] = sc.nextInt();
             }
         }
-        
-        nextIdx = 0;
-        next = new int[m];
+
+        spare = new int[m];
         for(int i=0; i<m; i++) {
-            next[i] = sc.nextInt();
+            spare[i] = sc.nextInt();
         }
 
-        int answer;
         while(k-- > 0) {
-            answer = 0;
-            SpinResult maxResult = new SpinResult(0, null);
-            maxCount = 0;
-            
-            for(int d=2; d<=6; d+=2) {
-                for(int i=1; i<=3; i++) {
-                    for(int j=1; j<=3; j++) {
-                        SpinResult result = simulation(i, j, d);
-                        if(result != null && result.count > maxResult.count) {
-                            maxCount = result.count;
-                            maxResult = result;
-                        }
-                    } 
-                }
-            }
+            int totalFind = 0;
 
-            if(maxCount == 0) {
+            //최대 회전 찾기
+            int find = getMaxRotate();
+            if(find == 0) {
                 break;
+            } else {
+                totalFind += find;
             }
 
-            wallToAfter(maxResult.after);
-
-            //빈 곳 채워넣기
-            fill();
-
-            answer += maxResult.count;
-
-            //또 생긴 유물이 있나 체크
-            maxCount = 0;
-            SpinResult result = check();
-            while(result != null) {
-                answer += result.count;
-                wallToAfter(result.after);
+            //연쇄 유물 획득 ( + 빈자리 채우기)
+            FindResult findResult = find(map);
+            while(findResult.count != 0) {
+                totalFind += findResult.count;
+                erase(findResult.findMap);
                 fill();
-                maxCount = 0;
-                result = check();                
+                findResult = find(map);
             }
-            System.out.print(answer + " ");
+            System.out.print(totalFind + " ");
         }
     }
 
-    static void wallToAfter(int[][] after) {
-        for(int i=0; i<5; i++) {
-            for(int j=0; j<5; j++) {
-                wall[i][j] = after[i][j];
+    static int getMaxRotate() {
+        int maxCount = 0;
+        int[][] maxFindMap = null;
+        int[][] maxRotatedMap = null;
+
+        //순회하면서 최대 위치와 각도 찾기
+        for(int d=2; d<=6; d+=2) {
+            for(int i=1; i<=3; i++) {
+                for(int j=1; j<=3; j++) {
+                    int[][] rotatedMap = rotate(i, j, d);
+                    FindResult findResult = find(rotatedMap);
+                    if(findResult.count > maxCount) {
+                        maxCount = findResult.count;
+                        maxFindMap = findResult.findMap;
+                        maxRotatedMap = rotatedMap;
+                    }
+                }
             }
         }
+
+        if(maxCount == 0) {
+            return 0;
+        }
+        
+        map = maxRotatedMap;
+
+        //유물 획득
+        erase(maxFindMap);
+
+        //빈자리 채우기
+        fill();
+
+        return maxCount;
     }
-    
+
     static void fill() {
-        for(int j=0; j<5;j++) {
-            for(int i=4; i>=0; i--) {
-                if(wall[i][j] == 0) {
-                    wall[i][j] = next[nextIdx++];
+        for(int j=0; j<N; j++) {
+            for(int i=N-1; i>=0; i--) {
+                if(map[i][j] == 0) {
+                    map[i][j] = spare[spareIdx++];
                 }
             }
         }
     }
 
-    static class SpinResult {
-        int count;
-        int[][] after;
-        
-        SpinResult(int count, int[][] after) {
-            this.count = count;
-            this.after = after;
-        }
-    }
-
-    static SpinResult simulation(int x, int y, int d) {
-        int[][][] part = spin(x, y, d);
-        
-        //벽 돌리기
-        change(x, y, part[0]);
-
-        //유물 확인
-        SpinResult result = check();
-
-        //벽 원상태로
-        change(x, y, part[1]);
-        
-        return result;
-    }
-
-    static SpinResult check() {
-        int[][] visited = new int[5][5];
-        int totalCount = 0;
-        for(int i=0; i<5; i++) {
-            for(int j=0; j<5; j++) {
-                if(visited[i][j] != 0) {
-                    continue;
-                }
-                int curr = wall[i][j];
-                ArrayList<int[]> list = new ArrayList<>();
-                Queue<int[]> que = new LinkedList<>();
-                
-                que.offer(new int[]{i,j});
-                list.add(new int[]{i,j});
-                visited[i][j] = 1;
-            
-                int count = 1;
-                while(!que.isEmpty()) {
-                    int[] poll = que.poll();
-                    
-                    for(int d=0; d<8; d+=2) {
-                        int nx = poll[0] + dx[d];
-                        int ny = poll[1] + dy[d];
-
-                        if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || visited[nx][ny] != 0 || wall[nx][ny] != curr) {
-                            continue;
-                        }
-                        count++;
-                        visited[nx][ny] = 1;
-                        que.offer(new int[]{nx, ny});
-                        list.add(new int[]{nx, ny});
-                    }
-                }
-
-                if(count >= 3) {
-                    totalCount += count;
-                    for(int[] coor : list) {
-                        visited[coor[0]][coor[1]] = 2;
-                    }
+    static void erase(int[][] findMap) {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(findMap[i][j] == 2) {
+                    map[i][j] = 0;
                 }
             }
         }
-
-        if(totalCount > maxCount) {
-            int[][] after = new int[5][5];
-            for(int i=0; i<5; i++) {
-                for(int j=0; j<5; j++) {
-                    if(visited[i][j] == 2) {
-                        after[i][j] = 0;
-                    } else {
-                        after[i][j] = wall[i][j];
-                    }
-                }
-            }
-            return new SpinResult(totalCount, after);
-        }
-        return null;
     }
 
-    static void change(int x, int y, int[][] part) {
-        int partX = 1;
-        int partY = 1;
-        for(int i=-1; i<=1; i++) {
-            for(int j=-1; j<=1; j++) {
-                wall[x + i][y + j] = part[partX + i][partY + j];
+    static int[][] rotate(int x, int y, int d) {
+        int[][] rotatedMap = new int[N][N];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                rotatedMap[i][j] = map[i][j];
             }
         }
-    }
 
-    static int[][][] spin(int x, int y, int d) {
-        int[][][] part = new int[2][3][3];
-        
-        int partX = 1;
-        int partY = 1;
-        part[0][partX][partY] = wall[x][y];
-        part[1][partX][partY] = wall[x][y];
-        
         for(int i=0; i<8; i++) {
-            for(int j=0; j<2; j++) {
-                int partNX = partX + dx[i];
-                int partNY = partY + dy[i];
-                
-                int nx, ny;
-                if(j == 0) {
-                    nx = x + dx[((i - d) < 0) ? 8 + (i - d) : i - d];
-                    ny = y + dy[((i - d) < 0) ? 8 + (i - d) : i - d];
-                } else {
-                    nx = x + dx[i];
-                    ny = y + dy[i];
+            int rotateX = x + DX[i];
+            int rotateY = y + DY[i];
+
+            int originX = x + DX[(8 + i - d) % 8];
+            int originY = y + DY[(8 + i - d) % 8];
+
+            rotatedMap[rotateX][rotateY] = map[originX][originY];
+        }
+        return rotatedMap;
+    }
+
+    static class FindResult {
+        int count;
+        int[][] findMap;
+
+        FindResult(int count, int[][] findMap) {
+            this.count = count;
+            this.findMap = findMap;
+        }
+    }
+
+    static FindResult find(int[][] targetMap) {
+        int[][] visited = new int[N][N];
+        int totalCount = 0;
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(visited[i][j] == 0) {
+                    int curr = targetMap[i][j];
+                    
+                    Queue<int[]> que = new LinkedList<>();
+                    List<int[]> list = new ArrayList<>();
+                    
+                    que.offer(new int[]{i, j});
+                    list.add(new int[]{i, j});
+                    visited[i][j] = 1;
+
+                    int count = 1;
+                    while(!que.isEmpty()) {
+                        int[] poll = que.poll();
+                        for(int d=0; d<8; d+=2) {
+                            int nx = poll[0] + DX[d];
+                            int ny = poll[1] + DY[d];
+                            
+                            if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] != 0 || targetMap[nx][ny] != curr) {
+                                continue;
+                            }
+
+                            count++;
+                            que.offer(new int[]{nx, ny});
+                            list.add(new int[]{nx, ny});
+                            visited[nx][ny] = 1;
+                        }
+                    }
+
+                    if(count >= 3) {
+                        totalCount += count;
+                        for(int[] coor : list) {
+                            visited[coor[0]][coor[1]] = 2;
+                        }
+                    }
                 }
-                part[j][partNX][partNY] = wall[nx][ny];
             }
         }
-        return part;
-    }
+        return new FindResult(totalCount, visited);
+    }    
 }
