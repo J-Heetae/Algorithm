d69788d00fb32c42c44dcb14f213a90258f1c23a
Jeong TaeHee <th1563@naver.com>
2024-12-04 16:31:00 +0900

    Title: 코드트리 오마카세, Time: 2589ms, Memory: 102MB, Status: Passed - CodeTree

--- 241204/코드트리 오마카세/codetree-omakase.java
+++ 241204/코드트리 오마카세/codetree-omakase.java
@@ -1,209 +1,110 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
 
-    static ArrayList<Integer> sushiXList;
-    static ArrayList<Integer> customerXList;
-    static HashMap<Integer, HashMap<String, Integer>> sushiMap;
-    static HashMap<Integer, HashMap<String, Integer>> customerMap;
+    static class Query implements Comparable<Query> {
+        int order;
+        int time;
+        int x;
+        String name;
+        
+        Query(int order, int time, int x, String name) {
+            this.order = order;
+            this.time = time;
+            this.x = x;
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(Query o) {
+            if(this.time == o.time) {
+                return this.order - o.order;
+            }
+            return this.time - o.time;
+        }
+    }
 
     static int L;
-    static int time, timeBefore;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+    static List<Query> q_list = new ArrayList<>();
+    static HashMap<String, List<Query>> p_q_map = new HashMap<>();
+    static HashMap<String, Integer> p_in_map = new HashMap<>();
+    static HashMap<String, Integer> p_x_map = new HashMap<>();
 
-        st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-        int Q = Integer.parseInt(st.nextToken());
-
-        sushiXList = new ArrayList<>();
-        customerXList = new ArrayList<>();
-        sushiMap = new HashMap<>();
-        customerMap = new HashMap<>();
-        
-        timeBefore = 1;
-        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        L = sc.nextInt();
+        int Q = sc.nextInt();
         while(Q-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int order = Integer.parseInt(st.nextToken());
-            time = Integer.parseInt(st.nextToken());
-        
-            //초밥 회전
-            sushiSpin();
-        
-            if(order == 100) { //초밥 놓기
-                int x = Integer.parseInt(st.nextToken());
-                String name = st.nextToken();
-                putSushi(x, name);
-                
+            int order = sc.nextInt();
+            int time = sc.nextInt();
+
+            if(order == 100) {
+                int x = sc.nextInt();
+                String name = sc.next();
+                Query query = new Query(order, time, x, name);
+                q_list.add(query);
+                p_q_map.computeIfAbsent(name, k -> new ArrayList<>()).add(query);
             }
 
-            if(order == 200) { //손님 착석
-                int x = Integer.parseInt(st.nextToken());
-                String name = st.nextToken();
-                int n = Integer.parseInt(st.nextToken());
-                customerIn(x, name, n);
+            if(order == 200) {
+                int x = sc.nextInt();
+                String name = sc.next();
+                int n = sc.nextInt();
+                Query query = new Query(order, time, x, name);
+                q_list.add(query);
+                p_in_map.put(name, time);
+                p_x_map.put(name, x);
             }
 
-            if(order == 300) { //사진 찍기
-                int customerCount = 0;
-                int sushiCount = 0;
-
-                for(int x : customerMap.keySet()) {
-                    customerCount += customerMap.get(x).size();
-                }
-
-                for(int x : sushiMap.keySet()) {
-                    HashMap<String, Integer> currSushiMap = sushiMap.get(x);
-                    for(String name : currSushiMap.keySet()) {
-                        sushiCount += currSushiMap.get(name);
-                    }
-                }
-
-                sb.append(customerCount).append(" ").append(sushiCount).append("\n");
+            if(order == 300) {
+                q_list.add(new Query(order, time, -1, ""));
             }
-            timeBefore = time;
         }
-        System.out.print(sb);
-    }
 
-    static void putSushi(int x, String name) {
-        int sushiX = (L + x - (time % L)) % L;
-        if(!sushiMap.containsKey(sushiX)) {
-            sushiMap.put(sushiX, new HashMap<>());
-            sushiXList.add(sushiX);
-        }
-        sushiMap.get(sushiX).put(name, sushiMap.get(sushiX).getOrDefault(name, 0) + 1);
-        eat(x, name);
-    }
+        for(String name : p_q_map.keySet()) {
+            List<Query> list = p_q_map.get(name);
 
-    static void customerIn(int x, String name, int n) {
-        if(!customerMap.containsKey(x)) {
-            customerMap.put(x, new HashMap<>());
-            customerXList.add(x);
-        }
-        customerMap.get(x).put(name, customerMap.get(x).getOrDefault(name, 0) + n);
-        eat(x, name);
-    }
-
-    static void eat(int x, String name) {
-        int sushiX = (L + x - (time % L)) % L;
-        
-        if(sushiMap.containsKey(sushiX) && customerMap.containsKey(x)) {
-            HashMap<String, Integer> currSushiMap = sushiMap.get(sushiX);
-            HashMap<String, Integer> currCusMap = customerMap.get(x);
+            int customer_x = p_x_map.get(name);
+            int in_time = p_in_map.get(name);
+            int end_time = 0;
             
-            if(currSushiMap.containsKey(name) && currCusMap.containsKey(name)) {
-                int made = currSushiMap.get(name);
-                int need = currCusMap.get(name);
-                
-                if(made == need) {
-                    currSushiMap.remove(name);
-                    currCusMap.remove(name);
-                } else if (made > need) {
-                    currSushiMap.put(name, (made - need));
-                    currCusMap.remove(name);
-                } else if (made < need) {
-                    currSushiMap.remove(name);
-                    currCusMap.put(name, (need - made));
+            for(Query q : list) {
+                int sushi_end_time = q.time;
+                int sushi_x = q.x;
+
+                if(q.time < in_time) { //스시가 먼저
+                    sushi_end_time = in_time; 
+                    sushi_x = (q.x + (in_time - q.time)) % L;
+
+                } else { //손님이 먼저
+                    sushi_end_time = q.time;
                 }
+
+                sushi_end_time += (customer_x - sushi_x + L) % L;
+                end_time = Math.max(end_time, sushi_end_time);
+                q_list.add(new Query(111, sushi_end_time, -1, ""));
             }
-
-            if(currSushiMap.size() == 0) {
-                sushiMap.remove(sushiX);
-                sushiXList.remove(Integer.valueOf(sushiX));
-            }
-
-            if(currCusMap.size() == 0) {
-                customerMap.remove(x);
-                customerXList.remove(Integer.valueOf(x));
-            }
-        }
-    }
-
-    static void sushiSpin() {
-        int range = (time - timeBefore >= (L - 1)) ? (L - 1) : time - timeBefore;
-
-        if(range == 0) {
-            return;
+            q_list.add(new Query(222, end_time, -1, ""));
         }
 
-        ArrayList<Integer> removeSushiX = new ArrayList<>();
-        ArrayList<Integer> removeCusX = new ArrayList<>();
-
-        //음식기준으로 돌아야함
-        for(int sushiX : sushiXList) {
-            int fixedSushiX = (sushiX + (timeBefore % L )) % L;
-            int fromX = fixedSushiX + 1;
-            int toX = (fixedSushiX + range) % L;
-
-            HashMap<String, Integer> currSushiMap = sushiMap.get(sushiX);
-            
-            for(int customerX : customerXList) {
-                if(toX > fromX) {
-                    if(customerX < fromX || customerX > toX) {
-                        continue;
-                    }
-                } else if (fromX > toX) {
-                    if(toX < customerX && customerX < fromX) {
-                        continue;
-                    }
-                } else {
-                    if(customerX > toX || customerX != fromX) {
-                        continue;
-                    }
-                }
-
-                //범위에 포함됨
-                HashMap<String, Integer> currCusMap = customerMap.get(customerX);
-
-                ArrayList<String> removeSushi = new ArrayList<>();
-
-                for(String name : currSushiMap.keySet()) {
-                    if(!currCusMap.containsKey(name)) {
-                        continue;
-                    }
-                    int made = currSushiMap.get(name);
-                    int need = currCusMap.get(name);
-
-                    if(made == need) {
-                        removeSushi.add(name);
-                        currCusMap.remove(name);
-                    } else if (made > need) {
-                        currSushiMap.put(name, (made - need));
-                        currCusMap.remove(name);
-                    } else if (made < need) {
-                        removeSushi.add(name);
-                        currCusMap.put(name, (need - made));
-                    }
-                }
-
-                if(currCusMap.size() == 0) {
-                    removeCusX.add(customerX);
-                }
-
-                for(String name : removeSushi) {
-                    currSushiMap.remove(name);
-                }
-            }
-
-            if(currSushiMap.size() == 0) {
-                removeSushiX.add(sushiX);
-            }
-        }
-
-        for(Integer x : removeSushiX) {
-            sushiMap.remove(x);
-            sushiXList.remove(x);
-        }
+        Collections.sort(q_list);
         
-        for(Integer x : removeCusX) {
-            customerMap.remove(x);
-            customerXList.remove(x);
+        int sushi_count = 0;
+        int customer_count = 0;
+        
+        for(Query q : q_list) {
+            if(q.order == 100) {
+                sushi_count++;
+            } else if (q.order == 111) {
+                sushi_count--;
+            } else if (q.order == 200) {
+                customer_count++;
+            } else if (q.order == 222) {
+                customer_count--;
+            } else {
+                System.out.println(customer_count + " " + sushi_count);
+            }
         }
     }
 }
