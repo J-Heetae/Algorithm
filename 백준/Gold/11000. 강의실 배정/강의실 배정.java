0cf3a698741b5a18502798f8bfd011dd8f67d6ae
Jeong TaeHee <th1563@naver.com>
2025-08-23 08:34:27 +0900

    [Gold IV] Title: 강의실 배정, Time: 656 ms, Memory: 72960 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,38 +1,42 @@
 import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
 
-        int[][] classes = new int[N][2];
-
-        for(int i = 0; i < N; i++) {
-            classes[i][0] = sc.nextInt();
-            classes[i][1] = sc.nextInt();
+        int[][] lectures = new int[N][2];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            lectures[i] = new int[] {
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())};
         }
-        Arrays.sort(classes, (a,b) -> {
-            if(a[0] == b[0]) {
-                return a[1] - b[1];
-            }
+        Arrays.sort(lectures, (a, b) -> {
+            if(a[0] == b[0]) return a[1] - b[1];
             return a[0] - b[0];
         });
 
-        int room = 1;
+        int answer = 1;
         PriorityQueue<Integer> endTime = new PriorityQueue<>();
-        for(int[] c : classes) {
-            if(endTime.isEmpty() || endTime.peek() > c[0]) {
-                if(room <= endTime.size()) {
-                    room++;
-                }
-                endTime.offer(c[1]);
+        for(int[] lec : lectures) {
+            if(endTime.isEmpty()) {
+                endTime.offer(lec[1]);
                 continue;
             }
-            while(!endTime.isEmpty() && endTime.peek() <= c[0]) {
+
+            if(endTime.peek() <= lec[0]) {
                 endTime.poll();
+                endTime.offer(lec[1]);
+            } else {
+                if(endTime.size() == answer) {
+                    answer++;
+                    endTime.offer(lec[1]);
+                }
             }
-            endTime.offer(c[1]);
         }
-        System.out.println(room);
+        System.out.print(answer);
     }
 }
