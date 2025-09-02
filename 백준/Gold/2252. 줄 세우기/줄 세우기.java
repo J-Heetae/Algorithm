e3f504a02d846a12c1bb3b48c6e9c9da5d78e492
Jeong TaeHee <th1563@naver.com>
2025-09-03 00:20:37 +0900

    [Gold III] Title: 줄 세우기, Time: 460 ms, Memory: 46364 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] in = new int[N + 1];
+        List<Integer>[] out = new List[N + 1];
+        for(int i=1; i<=N; i++) {
+            out[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<M ;i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            in[b]++;
+            out[a].add(b);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i<=N; i++) {
+            if(in[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!q.isEmpty()) {
+            int poll = q.poll();
+            sb.append(poll).append(" ");
+            for(int next : out[poll]) {
+                in[next]--;
+                if(in[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+        System.out.print(sb);
+    }
+}
