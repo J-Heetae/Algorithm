38da09984ae88867933d3222562cf6e8bfa2febc
Jeong TaeHee <th1563@naver.com>
2025-09-23 21:14:26 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,49 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(reader.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         StringBuilder answer = new StringBuilder();
-
-        while (T-- > 0) {
-            int n = Integer.parseInt(reader.readLine());
-
-            if (n == 0) {
-                answer.append(0).append("\n");
-                continue;
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            Map<String, Integer> map = new HashMap<>();
+            while (n-- > 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String type = st.nextToken();
+                map.put(type, map.getOrDefault(type, 0) + 1);
             }
-
-            Map<String, Integer> categoryCount = new HashMap<>();
-
-            for (int i = 0; i < n; i++) {
-                StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-                String item = tokenizer.nextToken();
-                String category = tokenizer.nextToken();
-
-                categoryCount.put(category, categoryCount.getOrDefault(category, 0) + 1);
+            int num = 1;
+            for (int v : map.values()) {
+                num *= v + 1;
             }
-
-            int combinations = 1;
-
-            for (int count : categoryCount.values()) {
-                combinations *= (count + 1); // 각 카테고리별로 선택하지 않는 경우를 포함
-            }
-
-            answer.append(combinations - 1).append("\n"); // 아무것도 선택하지 않는 경우 제외
+            answer.append(num - 1).append("\n");
         }
-
         System.out.println(answer);
     }
 }
