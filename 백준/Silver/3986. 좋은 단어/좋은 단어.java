d63ac87b85ede5cff19aaf63f0e0ad9499426680
Jeong TaeHee <th1563@naver.com>
2025-10-16 22:49:23 +0900

    [Silver IV] Title: 좋은 단어, Time: 276 ms, Memory: 23032 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int answer = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            Stack<Character> stack = new Stack<>();
+            for (Character c : str.toCharArray()) {
+                if (!stack.isEmpty() && stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
